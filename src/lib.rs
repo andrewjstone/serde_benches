8b6459c000fcc560f1feb8d027a92c0c99cbefc7
Andrew J. Stone <andrew.j.stone.1@gmail.com>
2017-05-11 23:43:34 -0400

    Add cbor and json benchmmarks

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,10 @@ extern crate serde_derive;
 extern crate rmp_serde as rmps;
 extern crate test;
 extern crate bincode;
+extern crate serde_cbor;
+
+#[macro_use]
+extern crate serde_json;
 
 use std::sync::Arc;
 use test::Bencher;
@@ -13,25 +17,42 @@ use serde::{Deserialize, Serialize};
 use rmps::{Deserializer, Serializer};
 use std::borrow::Cow;
 use bincode::{serialize, deserialize, Infinite};
+use serde_cbor::{to_vec, from_slice, ser};
 
-#[derive(Clone, Eq, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct NodeId {
     pub name: String,
     pub addr: String
 }
 
-#[derive(Clone, Eq, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
 pub struct Pid {
     pub group: Option<String>,
     pub name: String,
     pub node: NodeId
 }
 
-#[derive(Clone, Serialize, Deserialize)]
-pub struct Envelope {
-    pid1: Pid,
-    pid2: Pid,
-    pid3: Pid
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct PidV2 {
+    pub group: Option<String>,
+    pub name: String,
+    pub node: NodeId,
+    #[serde(default)]
+    pub owner: Option<String>
+}
+
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub enum Msg {
+    NodeId(NodeId),
+    Pid(Pid)
+}
+
+#[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub enum Msg2 {
+    NodeId(NodeId),
+    Pid(Pid),
+    Pid2(PidV2)
 }
 
 #[derive(Clone, Serialize, Deserialize)]
@@ -48,7 +69,7 @@ pub struct PidBorrowed<'a> {
 }
 
 #[bench]
-fn serialize_pid(b: &mut Bencher) {
+fn serialize_pid_msgpack(b: &mut Bencher) {
     let mut buf = Vec::new();
     let node = NodeId {
         name: "node1".to_owned(),
@@ -63,7 +84,129 @@ fn serialize_pid(b: &mut Bencher) {
 }
 
 #[bench]
-fn serialize_envelope(b: &mut Bencher) {
+fn serialize_pid_json(b: &mut Bencher) {
+    let node = NodeId {
+        name: "node1".to_owned(),
+        addr: "127.0.0.1:5000".to_owned()
+    };
+    let pid = Pid {
+        group: Some("haret".to_owned()),
+        name: "r1".to_owned(),
+        node: node
+    };
+    b.iter(|| serde_json::to_string(&pid))
+}
+
+#[bench]
+fn serialize_pid_cbor(b: &mut Bencher) {
+    let node = NodeId {
+        name: "node1".to_owned(),
+        addr: "127.0.0.1:5000".to_owned()
+    };
+    let pid = Pid {
+        group: Some("haret".to_owned()),
+        name: "r1".to_owned(),
+        node: node
+    };
+    b.iter(|| serde_cbor::to_vec(&pid))
+}
+
+#[bench]
+fn serialize_pid_cbor_packed(b: &mut Bencher) {
+    let node = NodeId {
+        name: "node1".to_owned(),
+        addr: "127.0.0.1:5000".to_owned()
+    };
+    let pid = Pid {
+        group: Some("haret".to_owned()),
+        name: "r1".to_owned(),
+        node: node
+    };
+    b.iter(|| serde_cbor::ser::to_vec_packed(&pid))
+}
+
+#[bench]
+fn serialize_pid_bincode() {
+    let node = NodeId {
+        name: "node1".to_owned(),
+        addr: "127.0.0.1:5000".to_owned()
+    };
+    let pid = Pid {
+        group: Some("haret".to_owned()),
+        name: "r1".to_owned(),
+        node: node
+    };
+    b.iter(|| serialize(&pid, Infinite).unwrap())
+}
+
+#[test]
+fn deserialize_old_version_bincode() {
+    let node = NodeId {
+        name: "node1".to_owned(),
+        addr: "127.0.0.1:5000".to_owned()
+    };
+    let pid = Pid {
+        group: Some("haret".to_owned()),
+        name: "r1".to_owned(),
+        node: node
+    };
+
+    let encoded = serialize(&pid, Infinite).unwrap();
+    let pid_v2: PidV2 = deserialize(&encoded[..]).unwrap();
+    println!("{:?}", pid_v2);
+}
+
+#[test]
+fn deserialize_old_version_json() {
+    let node = NodeId {
+        name: "node1".to_owned(),
+        addr: "127.0.0.1:5000".to_owned()
+    };
+    let pid = Pid {
+        group: Some("haret".to_owned()),
+        name: "r1".to_owned(),
+        node: node
+    };
+
+    let j = serde_json::to_string(&pid).unwrap();
+    let pid_v2: PidV2 = serde_json::from_str(&j).unwrap();
+    println!("{:?}", pid_v2);
+}
+
+#[test]
+fn deserialize_old_version_cbor() {
+    let node = NodeId {
+        name: "node1".to_owned(),
+        addr: "127.0.0.1:5000".to_owned()
+    };
+    let pid = Pid {
+        group: Some("haret".to_owned()),
+        name: "r1".to_owned(),
+        node: node
+    };
+
+    let j = serde_cbor::to_vec(&pid).unwrap();
+    let pid_v2: PidV2 = serde_cbor::from_slice(&j).unwrap();
+}
+
+#[test]
+fn deserialize_old_version_cbor_packed() {
+    let node = NodeId {
+        name: "node1".to_owned(),
+        addr: "127.0.0.1:5000".to_owned()
+    };
+    let pid = Pid {
+        group: Some("haret".to_owned()),
+        name: "r1".to_owned(),
+        node: node
+    };
+
+    let j = serde_cbor::ser::to_vec_packed(&pid).unwrap();
+    let pid_v2: PidV2 = serde_cbor::from_slice(&j).unwrap();
+}
+
+#[test]
+fn deserialize_old_version_msgpack() {
     let mut buf = Vec::new();
     let node = NodeId {
         name: "node1".to_owned(),
@@ -74,35 +217,14 @@ fn serialize_envelope(b: &mut Bencher) {
         name: "r1".to_owned(),
         node: node
     };
-    let envelope = Envelope {
-        pid1: pid.clone(),
-        pid2: pid.clone(),
-        pid3: pid
-    };
-    b.iter(|| envelope.serialize(&mut Serializer::new(&mut buf)))
+
+    pid.serialize(&mut Serializer::new(&mut buf)).unwrap();
+    let mut de = Deserializer::new(&buf[..]);
+    let pid_v2: PidV2 = Deserialize::deserialize(&mut de).unwrap();
 }
 
 #[bench]
-fn serialize_envelope_bincode(b: &mut Bencher) {
-    let node = NodeId {
-        name: "node1".to_owned(),
-        addr: "127.0.0.1:5000".to_owned()
-    };
-    let pid = Pid {
-        group: Some("haret".to_owned()),
-        name: "r1".to_owned(),
-        node: node
-    };
-    let envelope = Envelope {
-        pid1: pid.clone(),
-        pid2: pid.clone(),
-        pid3: pid
-    };
-    b.iter(|| serialize(&envelope, Infinite).unwrap())
-}
-
-#[bench]
-fn serialize_arc_pid(b: &mut Bencher) {
+fn serialize_arc_pid_msgpack(b: &mut Bencher) {
     let mut buf = Vec::new();
     let node = NodeId {
         name: "node1".to_owned(),
@@ -117,7 +239,7 @@ fn serialize_arc_pid(b: &mut Bencher) {
 }
 
 #[bench]
-fn serialize_cow_pid(b: &mut Bencher) {
+fn serialize_cow_pid_msgpack(b: &mut Bencher) {
     let mut buf = Vec::new();
     let node = NodeId {
         name: "node1".to_owned(),
@@ -132,7 +254,7 @@ fn serialize_cow_pid(b: &mut Bencher) {
 }
 
 #[bench]
-fn deserialize_pid(b: &mut Bencher) {
+fn deserialize_pid_msgpack(b: &mut Bencher) {
     let mut buf = Vec::new();
     let node = NodeId {
         name: "node1".to_owned(),
@@ -152,8 +274,7 @@ fn deserialize_pid(b: &mut Bencher) {
 }
 
 #[bench]
-fn deserialize_envelope(b: &mut Bencher) {
-    let mut buf = Vec::new();
+fn deserialize_pid_json(b: &mut Bencher) {
     let node = NodeId {
         name: "node1".to_owned(),
         addr: "127.0.0.1:5000".to_owned()
@@ -163,21 +284,12 @@ fn deserialize_envelope(b: &mut Bencher) {
         name: "r1".to_owned(),
         node: node
     };
-    let envelope = Envelope {
-        pid1: pid.clone(),
-        pid2: pid.clone(),
-        pid3: pid
-    };
-    envelope.serialize(&mut Serializer::new(&mut buf)).unwrap();
-    b.iter(|| {
-        let mut de = Deserializer::new(&buf[..]);
-        let env: Envelope = Deserialize::deserialize(&mut de).unwrap();
-        env
-    })
+    let j = serde_json::to_string(&pid).unwrap();
+    b.iter(|| serde_json::from_str::<Pid>(&j))
 }
 
 #[bench]
-fn deserialize_envelope_bincode(b: &mut Bencher) {
+fn deserialize_pid_cbor(b: &mut Bencher) {
     let node = NodeId {
         name: "node1".to_owned(),
         addr: "127.0.0.1:5000".to_owned()
@@ -187,20 +299,83 @@ fn deserialize_envelope_bincode(b: &mut Bencher) {
         name: "r1".to_owned(),
         node: node
     };
-    let envelope = Envelope {
-        pid1: pid.clone(),
-        pid2: pid.clone(),
-        pid3: pid
+    let j = serde_cbor::to_vec(&pid).unwrap();
+    b.iter(|| serde_cbor::from_slice::<Pid>(&j))
+}
+
+#[bench]
+fn deserialize_pid_cbor_packed(b: &mut Bencher) {
+    let node = NodeId {
+        name: "node1".to_owned(),
+        addr: "127.0.0.1:5000".to_owned()
     };
-    let v: Vec<u8> = serialize(&envelope, Infinite).unwrap();
+    let pid = Pid {
+        group: Some("haret".to_owned()),
+        name: "r1".to_owned(),
+        node: node
+    };
+    let j = serde_cbor::ser::to_vec_packed(&pid).unwrap();
+    b.iter(|| serde_cbor::from_slice::<Pid>(&j))
+}
+
+
+#[bench]
+fn deserialize_pid_bincode(b: &mut Bencher) {
+    let node = NodeId {
+        name: "node1".to_owned(),
+        addr: "127.0.0.1:5000".to_owned()
+    };
+    let pid = Pid {
+        group: Some("haret".to_owned()),
+        name: "r1".to_owned(),
+        node: node
+    };
+    let v: Vec<u8> = serialize(&pid, Infinite).unwrap();
     b.iter(|| {
-        let env: Envelope = deserialize(&v[..]).unwrap();
+        let env: Pid = deserialize(&v[..]).unwrap();
         env
     })
 }
 
 #[bench]
-fn serialize_pid_borrowed(b: &mut Bencher) {
+fn deserialize_pid_borrowed_bincode(b: &mut Bencher) {
+    let node = NodeIdBorrowed {
+        name: "node1",
+        addr: "127.0.0.1:5000"
+    };
+    let pid = PidBorrowed {
+        group: Some("haret"),
+        name: "r1",
+        node: node
+    };
+    let v: Vec<u8> = serialize(&pid, Infinite).unwrap();
+    b.iter(|| {
+        let env: PidBorrowed = deserialize(&v[..]).unwrap();
+        env
+    })
+}
+
+#[bench]
+fn deserialize_old_enum_into_new_bincode(b: &mut Bencher) {
+    let node = NodeId {
+        name: "node1".to_owned(),
+        addr: "127.0.0.1:5000".to_owned()
+    };
+    let pid = Pid {
+        group: Some("haret".to_owned()),
+        name: "r1".to_owned(),
+        node: node
+    };
+    let msg = Msg::Pid(pid);
+    let v: Vec<u8> = serialize(&msg, Infinite).unwrap();
+    b.iter(|| {
+        let msg: Msg2 = deserialize(&v[..]).unwrap();
+        msg
+    })
+}
+
+#[bench]
+fn serialize_pid_borrowed_msgpack(b: &mut Bencher) {
     let mut buf = Vec::new();
     let node = NodeIdBorrowed {
         name: "node1",
@@ -215,7 +390,7 @@ fn serialize_pid_borrowed(b: &mut Bencher) {
 }
 
 #[bench]
-fn deserialize_pid_borrowed(b: &mut Bencher) {
+fn deserialize_pid_borrowed_msgpack(b: &mut Bencher) {
     let mut buf = Vec::new();
     let node = NodeIdBorrowed {
         name: "node1",
@@ -232,6 +407,21 @@ fn deserialize_pid_borrowed(b: &mut Bencher) {
         let pid: Result<PidBorrowed, rmps::decode::Error> = Deserialize::deserialize(&mut de);
         pid
     })
+}
+
+#[bench]
+fn deserialize_pid_borrowed_json(b: &mut Bencher) {
+    let node = NodeIdBorrowed {
+        name: "node1",
+        addr: "127.0.0.1:5000"
+    };
+    let pid = PidBorrowed {
+        group: Some("haret"),
+        name: "r1",
+        node: node
+    };
+    let serialized = serde_json::to_string(&pid).unwrap();
+    b.iter(|| serde_json::from_str::<PidBorrowed>(&serialized))
 }
 
 #[bench]
